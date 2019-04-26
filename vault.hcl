2e138c7fe25a3a26ab671cec7be066514511e6e3
pzombade <prashant_zombade@bmc.com>
2019-04-26 18:05:01 +0530

    Adding initial files

---
+++ vault.hcl
@@ -0,0 +1,27 @@
+ui=true
+
+api_addr = "http://IP_ADDRESS:8200"
+cluster_addr = "https://IP_ADDRESS:8201"
+
+storage "file"{
+path = "/root/vault/vault_file_storage2"
+}
+
+listener "tcp" {
+   cluster_address  = "IP_ADDRESS:8201"
+   address     = "0.0.0.0:8200"
+   tls_disable = 1
+}
+
+path "sys/mounts/*" {
+  capabilities = [ "create", "read", "update", "delete", "list" ]
+}
+
+path "sys/mounts" {
+  capabilities = [ "read", "list" ]
+}
+
+path "pki*" {
+  capabilities = [ "create", "read", "update", "delete", "list", "sudo" ]
+}
+
---
+++ vault_installation.sh
@@ -0,0 +1,18 @@
+IPADD=$(hostname -i)
+echo "The IPD add is $IPADD"
+sed 's/HOST/$IPADD/g' vault.hcl
+wget https://releases.hashicorp.com/vault/1.1.1/vault_1.1.1_linux_amd64.zip
+unzip vault_1.1.1_linux_amd64.zip
+sudo chown root:root vault
+sudo mv vault /usr/local/bin/
+vault --version
+vault -autocomplete-install
+complete -C /usr/local/bin/vault vault
+#sudo setcap cap_ipc_lock=+ep /usr/local/bin/vault
+#sudo useradd --system --home /etc/vault.d --shell /bin/false vault
+sudo mv vault.service /etc/systemd/system
+sudo mkdir -p /etc/vault.d/vault_storage
+sudo mv vault.hcl /etc/vault.d
+sudo chown --recursive vault:vault /etc/vault.d
+sudo chmod 640 /etc/vault.d/vault.hcl
+vault server -config=/etc/vault.d/vault.hcl
