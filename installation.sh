f156379ab9112590253c57fb6c46460479017774
pzombade <prashant_zombade@bmc.com>
2019-04-29 15:27:20 +0530

    Updated installation.sh to executed system commands

--- installation.sh
+++ installation.sh
@@ -17,11 +17,11 @@ unzip vault_1.1.1_linux_amd64.zip
 sudo chown root:root vault
 sudo mv vault /usr/local/bin/
 vault --version
-#vault -autocomplete-install
-#complete -C /usr/local/bin/vault vault
-#sudo setcap cap_ipc_lock=+ep /usr/local/bin/vault
-#sudo useradd --system --home /etc/vault.d --shell /bin/false vault
+vault -autocomplete-install
+complete -C /usr/local/bin/vault vault
+sudo setcap cap_ipc_lock=+ep /usr/local/bin/vault
+sudo useradd --system --home /etc/vault.d --shell /bin/false vault
 
-#sudo chown --recursive vault:vault /etc/vault.d
+sudo chown --recursive vault:vault /etc/vault.d
 sudo chmod 640 /etc/vault.d/vault.hcl
 vault server -config=/etc/vault.d/vault.hcl
