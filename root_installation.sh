8ca34f26d7872e67873a911a57d5ccb29edd61ca
pzombade <prashant_zombade@bmc.com>
2019-04-30 15:51:01 +0530

    Created root_installation.sh

---
+++ root_installation.sh
@@ -0,0 +1,15 @@
+wget https://releases.hashicorp.com/vault/1.1.1/vault_1.1.1_linux_amd64.zip
+unzip vault_1.1.1_linux_amd64.zip
+chown root:root vault
+mv vault /usr/local/bin/
+vault --version
+mv vault.service /etc/systemd/system/vault.service
+
+mkdir --parents /etc/vault.d
+mv vault.hcl /etc/vault.d/vault.hcl
+chmod 640 /etc/vault.d/vault.hcl
+
+systemctl enable vault
+systemctl start vault
+systemctl status vault
+
