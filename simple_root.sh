241970c44d6f4401344f9b11ccf15cd440a94e3b
pzombade <prashant_zombade@bmc.com>
2019-05-03 11:57:16 +0530

    Add redirection of stream

--- simple_root.sh
+++ simple_root.sh
@@ -13,10 +13,6 @@ echo "Updated vault.hcl"
 
 mv vault.hcl /etc/vault.d/vault.hcl
 
-vault server -config=/etc/vault.d/vault.hcl &
+vault server -config=/etc/vault.d/vault.hcl >> capture.txt
 vault operator init -recovery-shares=5 -recovery-threshold=2 >> keys.txt
-
-
-
-
 
